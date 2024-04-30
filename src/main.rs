461b7b38cae78b3f1de644b3f9da796a750b0cfe
Almugra <muiz-erem@gmx.de>
2024-04-30 23:21:01 +0200

    remove anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use anyhow::Context as _;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
 use shuttle_runtime::SecretStore;
 use shuttle_serenity::ShuttleSerenity;
 
+pub mod error;
+pub use error::Error;
+
 struct Data {}
-type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[poise::command(slash_command)]
@@ -17,7 +18,7 @@ async fn hello(ctx: Context<'_>) -> Result<(), Error> {
 async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleSerenity {
     let discord_token = secret_store
         .get("DISCORD_TOKEN")
-        .context("'DISCORD_TOKEN' was not found")?;
+        .expect("'DISCORD_TOKEN' was not found");
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
